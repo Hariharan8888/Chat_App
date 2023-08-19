cd97f6ef9eb528712f8dd924e25eb4611da7b71e
Hariharan K <jadegahari08@gmail.com>
2023-08-18 23:15:21 -0700

    commit

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -8,7 +8,7 @@ const Navbar = () => {
 
   return (
     <div className='navbar'>
-      <span className='logo'>Hari chat</span>
+      <span className='logo'>Hari Chat</span>
       <div className='user'>
         <img src={currentUser.photoURL} alt="" />
         <span>{currentUser.displayName}</span>
